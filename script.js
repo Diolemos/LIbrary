ba862ecb0ea1cfa4efc3b21c0bdc3771977b8451
Dio Lemos <pedrodiogenes@outlook.com>
2022-03-09 10:41:57 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// this empty array will store all the books(objects)
+let myLibrary = []
+
+//constructor function to create the book objects
+
+function Book(genre, title, author, edition, pages, read) {
+
+}
+
+//Function to add the book object to the library array
+
+
+//Function to display the books in the library...
+//*****on a table, card or list******
+
+
