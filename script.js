a4fe17cfc10e48ee72aa8627c6337ccc087889f7
Dio Lemos <pedrodiogenes@outlook.com>
2022-03-09 10:58:38 +0000

    Update script.js
    Constructor function for the book objects

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ let myLibrary = []
 //constructor function to create the book objects
 
 function Book(genre, title, author, edition, pages, read) {
+    genre = this.genre;
+    title = this.title;
+    author = this.author;
+    edition = this.edition;
+    pages = this.pages;
+    read = this.read;
+    function info() {
+        let text = `The book ${title} is ${genre} work. was written by ${author}. It has ${pages}. This is its edition number ${edition}`
 
+        if (read) {
+            text += ` You have already read this title`
+        } else {
+            text += `You haven't fully read this title yet`
+        }
+    }
 }
 
 //Function to add the book object to the library array
